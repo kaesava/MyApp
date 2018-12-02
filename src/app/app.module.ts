575e14e0dd4453d20ae6d980e8bcbfba018f9083
Kesh <kaesava@gmail.com>
2018-12-02 17:17:29 +1100

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyComponentComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
