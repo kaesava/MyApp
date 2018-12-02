dff3c2acb7608e75aceab080172768f187e17808
Kesh <kaesava@gmail.com>
2018-12-02 17:43:14 +1100

    reversed add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComponentComponent],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
